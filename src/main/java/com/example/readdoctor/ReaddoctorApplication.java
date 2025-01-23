3e9c3a5f22f37e3bb5dc180d7d4c642b4dcfa7fb
DavidSebas20 <davidsebastian20@outlook.com>
2025-01-23 14:00:54 -0500

    :tada: initialize read doctor project with Spring Boot and Maven setup

---
+++ src/main/java/com/example/readdoctor/ReaddoctorApplication.java
@@ -0,0 +1,13 @@
+package com.example.readdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaddoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaddoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/readdoctor/ReaddoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaddoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
